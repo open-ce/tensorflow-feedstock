5916394bc1ac768863fbc4f1941b01666cce91b6
Nishidha Panpaliya <npanpa23@in.ibm.com>
2020-11-25 14:27:31 +0000

    Fixed build for cuda11

--- buildscripts/set_tensorflow_nvidia_bazelrc.sh
+++ buildscripts/set_tensorflow_nvidia_bazelrc.sh
@@ -33,11 +33,11 @@ ARCH=`uname -p`
 ##
 CUDA_OPTION_1=''
 if [[ "${ARCH}" == 'x86_64' ]]; then
-    CUDA_OPTION_1='sm_37,sm_52,sm_60,sm_61,sm_70,compute_75'
+    CUDA_OPTION_1='sm_37,sm_52,sm_60,sm_61,sm_70,compute_75,'
 fi
 if [[ "${ARCH}" == 'ppc64le' ]]; then
     ## M40 and P4 never fully qualified on ppc64le
-    CUDA_OPTION_1='sm_37,sm_60,sm_70,compute_75'
+    CUDA_OPTION_1='sm_37,sm_60,sm_70,compute_75,'
 fi
 
 CUDA_VERSION="${cudatoolkit%.*}"
