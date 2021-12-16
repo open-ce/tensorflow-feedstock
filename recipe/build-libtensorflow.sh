27f3d1795223a7d78e79956f5eea04ff78878b3c
Nishidha <npanpa23@in.ibm.com>
2021-12-16 11:52:33 +0530

    Fix libtf (#71)
    * Fixed libtensorflow to make it noarch
    
    * Fixed cpu part of the recipe

--- recipe/build-libtensorflow.sh
+++ recipe/build-libtensorflow.sh
@@ -25,23 +25,27 @@ TF_MAJOR_VERSION=${PKG_VERSION:0:1}
 echo "TF_MAJOR_VERSION: $TF_MAJOR_VERSION"
 
 # Move libtensorflow libs in from SRC_DIR cache
-mv ${SRC_DIR}/tensorflow_pkg/libtensorflow.so ${SP_DIR}/tensorflow/
-mv ${SRC_DIR}/tensorflow_pkg/libtensorflow_cc.so ${SP_DIR}/tensorflow/
+
+TF_LIBDIR="${PREFIX}/lib"
+mkdir -p ${TF_LIBDIR}
+mv ${SRC_DIR}/tensorflow_pkg/libtensorflow.so ${TF_LIBDIR}/
+mv ${SRC_DIR}/tensorflow_pkg/libtensorflow_cc.so ${TF_LIBDIR}/
 
 #Create version sym links
-ln -s ${SP_DIR}/tensorflow/libtensorflow.so "${SP_DIR}/tensorflow/libtensorflow.so.${TF_MAJOR_VERSION}"
-ln -s ${SP_DIR}/tensorflow/libtensorflow_cc.so "${SP_DIR}/tensorflow/libtensorflow_cc.so.${TF_MAJOR_VERSION}"
+ln -s ${TF_LIBDIR}/libtensorflow.so "${TF_LIBDIR}/libtensorflow.so.${TF_MAJOR_VERSION}"
+ln -s ${TF_LIBDIR}/libtensorflow_cc.so "${TF_LIBDIR}/libtensorflow_cc.so.${TF_MAJOR_VERSION}"
 
 # Copy complete headers for libtensorflow C/C++ API
-mkdir -p "${SP_DIR}/tensorflow/include/tensorflow/cc"
-mkdir -p "${SP_DIR}/tensorflow/include/tensorflow/c"
-mkdir -p "${SP_DIR}/tensorflow/include/tensorflow/cc/ops"
+TF_INCDIR="${PREFIX}/include/tensorflow"
+mkdir -p "${TF_INCDIR}/cc"
+mkdir -p "${TF_INCDIR}/c"
+mkdir -p "${TF_INCDIR}/cc/ops"
 
 cd ./tensorflow/cc
-cp --parents `find -name \*.h*` "${SP_DIR}/tensorflow/include/tensorflow/cc"
+cp --parents `find -name \*.h*` "${TF_INCDIR}/cc"
 
 cd ../c
-cp --parents `find -name \*.h*` "${SP_DIR}/tensorflow/include/tensorflow/c"
+cp --parents `find -name \*.h*` "${TF_INCDIR}/c"
 
 cd ../../
 if [[ ! -d ${SRC_DIR}/tensorflow/include/tensorflow/cc/ops ]]
@@ -50,6 +54,6 @@ then
     echo "Please delete the previously generated tensorflow-base package from the output folder and rerun the build."
     exit 1
 else
-    cp -R "${SRC_DIR}/tensorflow/include/tensorflow/cc/ops/"*.h "${SP_DIR}/tensorflow/include/tensorflow/cc/ops"
+    cp -R "${SRC_DIR}/tensorflow/include/tensorflow/cc/ops/"*.h "${TF_INCDIR}/cc/ops"
 fi
 
