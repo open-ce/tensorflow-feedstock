9b5eca606f79940d97aa474d2bf6c8c40642ba9b
Nishidha <npanpa23@in.ibm.com>
2022-05-16 19:44:01 +0530

    GCC 11 support for P10 builds (#86)
    * GCC 11 support for P10 builds
    
    * Removed models patch

--- recipe/build-pip-package.sh
+++ recipe/build-pip-package.sh
@@ -19,14 +19,14 @@ set -vex
 
 if [[ $ppc_arch == "p10" ]]
 then 
-    if [[ -z "${GCC_10_HOME}" ]];
+    if [[ -z "${GCC_11_HOME}" ]];
     then
-	echo "Please set GCC_10_HOME to the install path of gcc-toolset-10"
+	echo "Please set GCC_11_HOME to the install path of gcc-toolset-11"
         exit 1
     else
-        export PATH=$GCC_10_HOME/bin:$PATH
-        export CC=$GCC_10_HOME/bin/gcc
-        export CXX=$GCC_10_HOME/bin/g++
+        export PATH=$GCC_11_HOME/bin:$PATH
+        export CC=$GCC_11_HOME/bin/gcc
+        export CXX=$GCC_11_HOME/bin/g++
         export BAZEL_LINKLIBS=-l%:libstdc++.a
     fi
 fi
